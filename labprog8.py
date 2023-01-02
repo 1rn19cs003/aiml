5be600f9fe305d561cceb70d973b71a26c92f83b
Abhishek Jaiswal <1rn19cs003@rnsit.onmicrosoft.com>
2023-01-02 09:18:01 +0530

    Update labprog8.py

--- labprog8.py
+++ labprog8.py
@@ -3,49 +3,22 @@
 #Print both correct and wrong predictions. Java/Python ML library classes can be used for
 #this problem.
 
-#import the dataset and library files
-from sklearn.datasets import load_iris
-from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
+from sklearn import datasets 
+from sklearn import metrics
+from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 
-iris_dataset=load_iris()
+iris = datasets.load_iris() 
+print(iris)
+X_train,X_test,y_train,y_test = train_test_split(iris.data,iris.target) 
+model =KMeans(n_clusters=3)
+model.fit(X_train,y_train) 
+model.score
+print('K-Mean: ',metrics.accuracy_score(y_test,model.predict(X_test)))
 
-#display the iris dataset
-print("\n IRIS FEATURES \ TARGET NAMES: \n ", iris_dataset.target_names)
-for i in range(len(iris_dataset.target_names)):
-    print("\n[{0}]:[{1}]".format(i,iris_dataset.target_names[i]))
-
-print("\n IRIS DATA :\n",iris_dataset["data"])
-
-#split the data into training and testing data
-X_train, X_test, y_train, y_test = train_test_split(iris_dataset["data"], iris_dataset["target"], random_state=0)
-
-print("\n Target :\n",iris_dataset["target"])
-print("\n X TRAIN \n", X_train)
-print("\n X TEST \n", X_test)
-print("\n Y TRAIN \n", y_train)
-print("\n Y TEST \n", y_test)
-
-#train and fit the model
-kn = KNeighborsClassifier(n_neighbors=5)
-kn.fit(X_train, y_train)
-
-#predicting from model
-x_new = np.array([[5, 2.9, 1, 0.2]])
-print("\n XNEW \n",x_new)
-prediction = kn.predict(x_new)
-print("\n Predicted target value: {}\n".format(prediction))
-print("\n Predicted feature name: {}\n".format(iris_dataset["target_names"][prediction]))
-
-i=1
-x= X_test[i]
-x_new = np.array([x])
-print("\n XNEW \n",x_new)
-
-for i in range(len(X_test)):
-  x = X_test[i]
-  x_new = np.array([x])
-  prediction = kn.predict(x_new)
-  print("\n Actual : {0} {1}, Predicted :{2}{3}".format(y_test[i],iris_dataset["target_names"][y_test[i]],prediction,iris_dataset["target_names"][ prediction]))
-print("\n TEST SCORE[ACCURACY]: {:.2f}\n".format(kn.score(X_test, y_test)))
+#-------Expectation and Maximization----------
+from sklearn.mixture import GaussianMixture 
+model2 = GaussianMixture(n_components=3) 
+model2.fit(X_train,y_train)
+model2.score
+print('EM Algorithm:',metrics.accuracy_score(y_test,model2.predict(X_test)))
