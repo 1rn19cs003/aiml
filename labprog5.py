dd907e082b7e23ab94efa85860d8955a1dad530d
Abhishek Jaiswal <1rn19cs003@rnsit.onmicrosoft.com>
2023-01-02 09:21:54 +0530

    Update labprog5.py

--- labprog5.py
+++ labprog5.py
@@ -1,55 +1,44 @@
 import numpy as np
-X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
-y = np.array(([92], [86], [89]), dtype=float)
-X = X/np.amax(X,axis=0) # maximum of X array longitudinally
-y = y/100
+inputNeurons=2 
+hiddenlayerNeurons=4 
+outputNeurons=2 
+iteration=6000
 
-#Sigmoid Function
-def sigmoid (x):
-    return 1/(1 + np.exp(-x))
+input = np.random.randint(1,5,inputNeurons) 
+output = np.array([1.0,0.0]) 
+hidden_layer=np.random.rand(1,hiddenlayerNeurons)
 
-#Derivative of Sigmoid Function
-def derivatives_sigmoid(x):
-    return x * (1 - x)
+hidden_biass=np.random.rand(1,hiddenlayerNeurons) 
+output_bias=np.random.rand(1,outputNeurons) 
+hidden_weights=np.random.rand(inputNeurons,hiddenlayerNeurons) 
+output_weights=np.random.rand(hiddenlayerNeurons,outputNeurons)
 
-#Variable initialization
-epoch=5000                #Setting training iterations
-lr=0.1                    #Setting learning rate
-inputlayer_neurons = 2    #number of features in data set
-hiddenlayer_neurons = 3   #number of hidden layers neurons
-output_neurons = 1        #number of neurons at output layer
+def sigmoid (layer):
+    return 1/(1 + np.exp(-layer))
 
-#weight and bias initialization
-wh=np.random.uniform(size=(inputlayer_neurons,hiddenlayer_neurons))
-bh=np.random.uniform(size=(1,hiddenlayer_neurons))
-wout=np.random.uniform(size=(hiddenlayer_neurons,output_neurons))
-bout=np.random.uniform(size=(1,output_neurons))
-#draws a random range of numbers uniformly of dim x*y
-for i in range(epoch):
-    
-#Forward Propogation
-    hinp1=np.dot(X,wh)
-    hinp=hinp1 + bh
-    hlayer_act = sigmoid(hinp)
-    outinp1=np.dot(hlayer_act,wout)
-    outinp= outinp1+ bout
-    output = sigmoid(outinp)
-    
-#Backpropagation
-    EO = y-output
-    outgrad = derivatives_sigmoid(output)
-    d_output = EO* outgrad
-    EH = d_output.dot(wout.T)
 
-#how much hidden layer wts contributed to error
-    hiddengrad = derivatives_sigmoid(hlayer_act)
-    d_hiddenlayer = EH * hiddengrad
-    
-# dotproduct of nextlayererror and currentlayerop
-    wout += hlayer_act.T.dot(d_output) *lr
-    wh += X.T.dot(d_hiddenlayer) *lr
-    
-    
-print("Input: \n" + str(X))
-print("Actual Output: \n" + str(y))
-print("Predicted Output: \n" ,output)
+def gradient(layer): 
+    return layer*(1-layer)
+
+for i in range(iteration):
+
+    hidden_layer=np.dot(input,hidden_weights) 
+    hidden_layer=sigmoid(hidden_layer+hidden_biass)
+
+    output_layer=np.dot(hidden_layer,output_weights) 
+    output_layer=sigmoid(output_layer+output_bias)
+
+    error = (output-output_layer) 
+    gradient_outputLayer=gradient(output_layer)
+    error_terms_output=gradient_outputLayer * error 
+    error_terms_hidden=gradient(hidden_layer)*np.dot(error_terms_output,output_weights.T)
+
+    gradient_hidden_weights = np.dot(input.reshape(inputNeurons,1),error_terms_hidden.reshape(1,hiddenlayerNeurons))
+    gradient_ouput_weights = np.dot(hidden_layer.reshape(hiddenlayerNeurons,1),error_terms_output.reshape(1,outputNeurons))
+
+    hidden_weights = hidden_weights + 0.05*gradient_hidden_weights 
+    output_weights = output_weights + 0.05*gradient_ouput_weights 
+    if i<50 or i>iteration-50:
+        print("**********************") 
+        print("iteration:",i,"::::",error) 
+        print("###output########",output_layer)
