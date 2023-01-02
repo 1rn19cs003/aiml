9c1ff5fe68d568e6e5b19351df5b0c3ba1e5eed5
Abhishek Jaiswal <1rn19cs003@rnsit.onmicrosoft.com>
2023-01-02 08:51:12 +0530

    Update labprog7.py

--- labprog7.py
+++ labprog7.py
@@ -11,105 +11,17 @@ import pandas as pd
 import numpy as np
 
 
-l1 = [0,1,2]
-def rename(s):
-	l2 = []
-	for i in s:
-		if i not in l2:
-			l2.append(i)
+iris = datasets.load_iris() 
+print(iris)
+X_train,X_test,y_train,y_test = train_test_split(iris.data,iris.target) 
+model =KMeans(n_clusters=3)
+model.fit(X_train,y_train) 
+model.score
+print('K-Mean: ',metrics.accuracy_score(y_test,model.predict(X_test)))
 
-	for i in range(len(s)):
-		pos = l2.index(s[i])
-		s[i] = l1[pos]
-	
-	return s
-
-# import some data to play with
-iris = datasets.load_iris()
-
-print("\n IRIS DATA :",iris.data);
-print("\n IRIS FEATURES :\n",iris.feature_names) 
-print("\n IRIS TARGET  :\n",iris.target) 
-print("\n IRIS TARGET NAMES:\n",iris.target_names)
-
-
-# Store the inputs as a Pandas Dataframe and set the column names
-X = pd.DataFrame(iris.data)
-
-#print(X)
-X.columns = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width']
-
-#print(X.columns) #print("X:",x)
-#print("Y:",y)
-y = pd.DataFrame(iris.target)
-y.columns = ['Targets']
-
-# Set the size of the plot
-plt.figure(figsize=(14,7))
-
-# Create a colormap
-colormap = np.array(['red', 'lime', 'black'])
-
-# Plot Sepal
-plt.subplot(1,2,1)
-plt.scatter(X.Sepal_Length,X.Sepal_Width, c=colormap[y.Targets], s=40)
-plt.title('Sepal')
-
-plt.subplot(1,2,2)
-plt.scatter(X.Petal_Length,X.Petal_Width, c=colormap[y.Targets], s=40)
-plt.title('Petal')
-plt.show()
-
-print("Actual Target is:\n", iris.target)
-
-# K Means Cluster
-model = KMeans(n_clusters=3)
-model.fit(X)
-
-# Set the size of the plot
-plt.figure(figsize=(14,7))
-
-# Create a colormap
-colormap = np.array(['red', 'lime', 'black'])
-
-# Plot the Original Classifications
-plt.subplot(1,2,1)
-plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[y.Targets], s=40)
-plt.title('Real Classification')
-
-# Plot the Models Classifications
-plt.subplot(1,2,2)
-plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[model.labels_], s=40)
-plt.title('K Mean Classification')
-plt.show()
-
-km = rename(model.labels_)
-print("\nWhat KMeans thought: \n", km)
-print("Accuracy of KMeans is ",sm.accuracy_score(y, km))
-print("Confusion Matrix for KMeans is \n",sm.confusion_matrix(y, km))
-
-#The GaussianMixture scikit-learn class can be used to model this problem 
-#and estimate the parameters of the distributions using the expectation-maximization algorithm.
-
-from sklearn import preprocessing
-scaler = preprocessing.StandardScaler()
-scaler.fit(X)
-xsa = scaler.transform(X)
-xs = pd.DataFrame(xsa, columns = X.columns)
-print("\n",xs.sample(5))
-
-from sklearn.mixture import GaussianMixture
-gmm = GaussianMixture(n_components=3)
-gmm.fit(xs)
-
-y_cluster_gmm = gmm.predict(xs)
-
-plt.subplot(1, 2, 1)
-plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[y_cluster_gmm], s=40)
-plt.title('GMM Classification')
-plt.show()
-
-em = rename(y_cluster_gmm)
-print("\nWhat EM thought: \n", em)
-print("Accuracy of EM is ",sm.accuracy_score(y, em))
-print("Confusion Matrix for EM is \n", sm.confusion_matrix(y, em))
+#-------Expectation and Maximization----------
+from sklearn.mixture import GaussianMixture 
+model2 = GaussianMixture(n_components=3) 
+model2.fit(X_train,y_train)
+model2.score
+print('EM Algorithm:',metrics.accuracy_score(y_test,model2.predict(X_test)))
