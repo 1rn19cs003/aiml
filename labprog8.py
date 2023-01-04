b6bd73a1e1135e52b5ba5e4b952af781a31f9e29
Abhishek Jaiswal <1rn19cs003@rnsit.onmicrosoft.com>
2023-01-04 18:07:10 +0530

    Update labprog8.py

--- labprog8.py
+++ labprog8.py
@@ -1,24 +1,17 @@
-# Python program to demonstrate # KNN classification algorithm # on IRISdataset
-#Write a program to implement k-Nearest Neighbour algorithm to classify the iris data set.
-#Print both correct and wrong predictions. Java/Python ML library classes can be used for
-#this problem.
+from sklearn.model_selection import train_test_split 
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn import datasets
+iris=datasets.load_iris() 
+print("Iris Data set loaded...")
+x_train, x_test, y_train, y_test = train_test_split(iris.data,iris.target,test_size=0.1)
+#random_state=0
+for i in range(len(iris.target_names)):
+    print("Label", i , "-",str(iris.target_names[i]))
+classifier = KNeighborsClassifier(n_neighbors=2)
+classifier.fit(x_train, y_train)
+y_pred=classifier.predict(x_test)
+print("Results of Classification using K-nn with K=1 ") 
+for r in range(0,len(x_test)):
+    print(" Sample:", str(x_test[r]), " Actual-label:", str(y_test[r])," Predicted-label:", str(y_pred[r]))
 
-from sklearn import datasets 
-from sklearn import metrics
-from sklearn.cluster import KMeans
-from sklearn.model_selection import train_test_split
-
-iris = datasets.load_iris() 
-print(iris)
-X_train,X_test,y_train,y_test = train_test_split(iris.data,iris.target) 
-model =KMeans(n_clusters=3)
-model.fit(X_train,y_train) 
-model.score
-print('K-Mean: ',metrics.accuracy_score(y_test,model.predict(X_test)))
-
-#-------Expectation and Maximization----------
-from sklearn.mixture import GaussianMixture 
-model2 = GaussianMixture(n_components=3) 
-model2.fit(X_train,y_train)
-model2.score
-print('EM Algorithm:',metrics.accuracy_score(y_test,model2.predict(X_test)))
+    print("Classification Accuracy :" , classifier.score(x_test,y_test));
