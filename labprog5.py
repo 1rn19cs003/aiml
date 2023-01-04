6643899c30ad2ea430a8ccf4ffe3601fa576af29
Abhishek Jaiswal <1rn19cs003@rnsit.onmicrosoft.com>
2023-01-04 19:24:47 +0530

    Update labprog5.py

--- labprog5.py
+++ labprog5.py
@@ -1,44 +1,35 @@
 import numpy as np
-inputNeurons=2 
-hiddenlayerNeurons=4 
-outputNeurons=2 
-iteration=6000
 
-input = np.random.randint(1,5,inputNeurons) 
-output = np.array([1.0,0.0]) 
-hidden_layer=np.random.rand(1,hiddenlayerNeurons)
-
-hidden_biass=np.random.rand(1,hiddenlayerNeurons) 
-output_bias=np.random.rand(1,outputNeurons) 
-hidden_weights=np.random.rand(inputNeurons,hiddenlayerNeurons) 
-output_weights=np.random.rand(hiddenlayerNeurons,outputNeurons)
-
-def sigmoid (layer):
-    return 1/(1 + np.exp(-layer))
-
-
-def gradient(layer): 
-    return layer*(1-layer)
-
-for i in range(iteration):
-
-    hidden_layer=np.dot(input,hidden_weights) 
-    hidden_layer=sigmoid(hidden_layer+hidden_biass)
-
-    output_layer=np.dot(hidden_layer,output_weights) 
-    output_layer=sigmoid(output_layer+output_bias)
-
-    error = (output-output_layer) 
-    gradient_outputLayer=gradient(output_layer)
-    error_terms_output=gradient_outputLayer * error 
-    error_terms_hidden=gradient(hidden_layer)*np.dot(error_terms_output,output_weights.T)
-
-    gradient_hidden_weights = np.dot(input.reshape(inputNeurons,1),error_terms_hidden.reshape(1,hiddenlayerNeurons))
-    gradient_ouput_weights = np.dot(hidden_layer.reshape(hiddenlayerNeurons,1),error_terms_output.reshape(1,outputNeurons))
-
-    hidden_weights = hidden_weights + 0.05*gradient_hidden_weights 
-    output_weights = output_weights + 0.05*gradient_ouput_weights 
-    if i<50 or i>iteration-50:
-        print("**********************") 
-        print("iteration:",i,"::::",error) 
-        print("###output########",output_layer)
+X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
+y = np.array(([92], [86], [89]), dtype=float)
+X = X/np.amax(X) # max of array
+y = y/100
+def sigmoid (x):
+     return 1/(1 + np.exp(-x))
+def derivatives_sigmoid(x):
+     return x * (1 - x)
+epoch=5000
+lr=0.1
+wh = np.random.uniform(size=(2,3)) 
+bh = np.random.uniform(size=(1,3))
+wout = np.random.uniform(size=(3,1))
+bout = np.random.uniform(size=(1,1))
+for i in range(epoch):
+     # forward prop
+     hinp=np.dot(X,wh) + bh
+     hlayer_act = sigmoid(hinp)
+     outinp=np.dot(hlayer_act,wout) + bout
+     output = sigmoid(outinp)
+     hiddengrad = derivatives_sigmoid(hlayer_act)
+     outgrad = derivatives_sigmoid(output)
+ 
+     EO = y-output
+     d_output = EO* outgrad
+ 
+     EH = d_output.dot(wout.T)
+     d_hiddenlayer = EH * hiddengrad
+     wout += hlayer_act.T.dot(d_output) *lr
+     wh += X.T.dot(d_hiddenlayer) *lr
+print("Input: \n" + str(X)) 
+print("Actual Output: \n" + str(y))
+print("Predicted Output: \n" ,output)
